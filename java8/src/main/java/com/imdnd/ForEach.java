d3966666d64a86dc8f86399d20675f4332127aeb
Adam DENG <dengnandong@yunzsp.com>
2017-05-05 10:07:55 +0800

    for each

---
+++ java8/src/main/java/com/imdnd/ForEach.java
@@ -0,0 +1,102 @@
+package com.imdnd;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by adam on 2017/5/5.
+ */
+public class ForEach {
+
+
+    public static void main(String[] args) {
+
+        ForEach forEach = new ForEach();
+        forEach.loopListWithJava8();
+        forEach.loopListWithoutJava8();
+        forEach.loopMapWithJava8();
+        forEach.loopMapWithoutJava8();
+
+    }
+
+    void loopMapWithoutJava8() {
+        Map<String, Integer> items = new HashMap<>();
+        items.put("A", 10);
+        items.put("B", 20);
+        items.put("C", 30);
+        items.put("D", 40);
+        items.put("E", 50);
+        items.put("F", 60);
+
+        for (Map.Entry<String, Integer> entry : items.entrySet()) {
+            System.out.println("Item : " + entry.getKey() + " Count : " + entry.getValue());
+        }
+    }
+
+
+    void loopMapWithJava8() {
+        Map<String, Integer> items = new HashMap<>();
+        items.put("A", 10);
+        items.put("B", 20);
+        items.put("C", 30);
+        items.put("D", 40);
+        items.put("E", 50);
+        items.put("F", 60);
+
+        items.forEach((k, v) -> System.out.println("Item : " + k + " Count : " + v));
+
+        items.forEach((k, v) -> {
+            System.out.println("Item : " + k + " Count : " + v);
+            if ("E".equals(k)) {
+                System.out.println("Hello E");
+            }
+        });
+    }
+
+
+    void loopListWithoutJava8() {
+        List<String> items = new ArrayList<>();
+        items.add("A");
+        items.add("B");
+        items.add("C");
+        items.add("D");
+        items.add("E");
+
+        for (String item : items) {
+            System.out.println(item);
+        }
+    }
+
+
+    void loopListWithJava8() {
+        List<String> items = new ArrayList<>();
+        items.add("A");
+        items.add("B");
+        items.add("C");
+        items.add("D");
+        items.add("E");
+
+        //lambda
+        //Output : A,B,C,D,E
+        items.forEach(item -> System.out.println(item));
+
+        //Output : C
+        items.forEach(item -> {
+            if ("C".equals(item)) {
+                System.out.println(item);
+            }
+        });
+
+        //method reference
+        //Output : A,B,C,D,E
+        items.forEach(System.out::println);
+
+        //Stream and filter
+        //Output : B
+        items.stream()
+                .filter(s -> s.contains("B"))
+                .forEach(System.out::println);
+    }
+}
